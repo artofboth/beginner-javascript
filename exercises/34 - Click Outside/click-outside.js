5df6fa27231d9a9ebfb9dfb61f124aae94b2f8a4
Julia Both <julia@artoboth.com>
2020-01-07 15:55:34 +1100

    Completed Click Outside Modal exercise
    Learnt about selecting and creating HTML elements as well as more about listening for user interaction.

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,19 +1,58 @@
-//Selecting our buttons
+// Selecting our buttons
 const cardButtons = document.querySelectorAll('.card button');
+const modal = document.querySelector('.modal-inner');
+const modalOuter = document.querySelector('.modal-outer');
 
-//Looping over individual buttons to add an event listener
-cardButtons.forEach(function attachListener(cardButton) {
-  cardButton.addEventListener('click', handleButtonClick);
-})
+// Grabbing our modal
+
+// Creating elements within the modal
+const modalImg = document.createElement('img');
+const modalTitle = document.createElement('h2');
+const modalTxt = document.createElement('p');
+
+modal.appendChild(modalImg);
+modal.appendChild(modalTitle);
+modal.appendChild(modalTxt);
 
 function handleButtonClick(e) {
-  //Grabbing data from the clicked card to display. 
+  // Grabbing data from the clicked card to display.
   const button = e.currentTarget;
   const card = button.closest('.card');
-  //This is like querySelector for whatever is closest to the element. 
+  // This is like querySelector for whatever is closest to the element.
   const imgSrc = card.querySelector('img').src;
-  const description = card.dataset.description;
-  console.log(description);
-  //Now populate the modal with this data. 
+  const title = card.querySelector('h2').innerHTML;
+  const { description } = card.dataset;
 
+  // Now populate the modal with this data.
+  modalImg.setAttribute('src', `${imgSrc.replace('200', '600')}`);
+  modalImg.setAttribute('width', '600');
+  modalImg.setAttribute('height', '600');
+  modalTitle.innerHTML = `${title}`;
+  modalTxt.innerHTML = `${description}`;
+
+  // Open the Modal
+  modalOuter.classList.add('open');
 }
+
+// Looping over individual buttons to add an event listener
+cardButtons.forEach(function attachListener(cardButton) {
+  cardButton.addEventListener('click', handleButtonClick);
+});
+
+function closeModal() {
+  modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', function(e) {
+  // Creating a boolean that detects if user is clicking inside our outside our element.
+  const isOutside = !e.target.closest('.modal-inner');
+  if (isOutside) {
+    closeModal();
+  }
+});
+
+window.addEventListener('keydown', e => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
