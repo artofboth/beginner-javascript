38d6e4bcb4236b7efab5893b48dea339b63ea64e
Júlia Both <julia@artofboth.com>
2020-01-08 20:29:50 +1100

    Scroll to Accept Exercise

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,34 @@
-console.log('IT WORKS!');
+//Placing the whole thing inside a function and quitting it if the element is not found is a way to prevent it executing in pages where the element isn't present. 
+
+function scrollToAccept() {
+
+    const terms = document.querySelector('.terms-and-conditions');
+
+    if (!terms) {
+        return;
+    };
+
+    const button = document.querySelector('.accept');
+
+    function obCallBack(payload) {
+        if (payload[0].intersectionRatio === 1) {
+            button.disabled = false;
+            //Stop observing the element afterwards
+            ob.unobserve(terms.lastElementChild);
+        };
+
+    }
+
+    const ob = new IntersectionObserver(obCallBack, {
+        root: terms,
+        threshold: 1,
+    });
+
+    ob.observe(terms.lastElementChild);
+
+    // terms.addEventListener('scroll', function (e) {
+    //     console.log(e);
+    // });
+};
+
+scrollToAccept();
