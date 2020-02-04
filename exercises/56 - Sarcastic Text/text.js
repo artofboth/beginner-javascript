958411a80f6d9c712d5ffc39ef50b8b6da0158f5
Julia Both <julia@artoboth.com>
2020-02-04 13:03:50 +1100

    wip #2  sarcastic text generator

--- exercises/56 - Sarcastic Text/text.js
+++ exercises/56 - Sarcastic Text/text.js
@@ -1,28 +1,44 @@
 console.log('it works');
 
+const text = document.querySelector('[name=text]');
+
 const handleClick = e => {
-  const filter = e.currentTarget.name;
-  console.log(e.currentTarget.value);
-  switch (filter) {
-    case 'sarcastic':
-      console.log(filter);
-      break;
-    case 'funky':
-      console.log(filter);
-      break;
-    case 'unable':
-      console.log(filter);
-      break;
-    default:
-      break;
+  const filter = e.currentTarget.id;
+  console.log(e.currentTarget.id);
+  const words = new Array(...text.value);
+
+  function filtering(char, i) {
+    if (filter === 'sarcastic') {
+      i % 3 === 0 ? (char = char.toUpperCase()) : char;
+    } else if (filter === 'funky') {
+      char = 'a';
+    } else {
+      char = 'u';
+    }
+    return char;
   }
+
+  const filteredWords = words.map(filtering);
+  const result = filteredWords.join('');
+  console.log(result);
 };
 
 const filters = document.querySelectorAll('[name=filter]');
+
 filters.forEach(function(e) {
   e.addEventListener('click', handleClick);
 });
 
-const text = document.querySelector('[name=text]');
+// function handleChange() {
+//   const words = new Array(...text.value);
 
-console.log(text);
+//   const filterWords = words.map(function uppercase(e, i) {
+//     i % 3 === 0 ? (e = e.toUpperCase()) : e;
+//     return e;
+//   });
+//   console.log(filterWords.join(''));
+// }
+
+// How can I make this function modular?
+
+// text.addEventListener('input', handleFilter('sarcastic'));
