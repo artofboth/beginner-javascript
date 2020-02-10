10e626909b7fd5f0b6c05c2ca956ee4efdd30d79
Julia Both <julia@artoboth.com>
2020-02-11 10:57:33 +1100

    finish sarcastic text code along

--- exercises/56 - Sarcastic Text/text_Code-Along.js
+++ exercises/56 - Sarcastic Text/text_Code-Along.js
@@ -5,7 +5,7 @@ const funkyLetters = {
   
 
 // 1. We grab all the relevant elements
-const textArea = document.querySelector('[name="text"]');
+const textArea = document.querySelector('[name="text"]'); // attribute selector
 const result = document.querySelector('.result');
 const filterInputs = Array.from(document.querySelectorAll('[name="filter"]'));
 
@@ -18,18 +18,48 @@ const filters = {
     // If this is even (i.e the result is zero), we will lowercase it.
     return char.toLowerCase();
   },
-  funky() {},
-  unable() {},
+  funky(letter) {
+    // Check if there is a corresponding funky letter
+    let funkyLetter = funkyLetters[letter];
+    // If yes return the funky letter
+    if (funkyLetter) return funkyLetter;
+    // If not check if there is a lowercase letter
+    funkyLetter = funkyLetters[letter.toLowerCase()];
+    if (funkyLetter) return funkyLetter;
+    // If not, return the regular letter
+    return letter;
+  },
+
+  unable(letter) {
+    // One in three spaces
+    const random = Math.floor(Math.random() * 3);
+    if (letter === ' ' && random === 2) {
+      return '...';
+    }
+    return letter;
+  },
 };
 
 // 2. We make a function that takes in a text parameter and transfmrs it.
 function transformText(text) {
+  // This query selector or filter will run every time there's an input.
   // const filter = document.querySelector('[name="filter"]:checked').value;
   const filter = filterInputs.find(input => input.checked).value;
-  console.log(filter);
   const mod = Array.from(text).map(filters[filter]);
   result.textContent = mod.join('');
+
+  console.log(text);
+  console.log(mod);
 }
 
 // 3. We listen for the user's input, then grab the result of that input and pass it on to our function that transforms text.
 textArea.addEventListener('input', e => transformText(e.target.value));
+
+// 4. Ensure the text transformation is triggered when user clicks checkboxes too.
+
+// We are looping over each input, running a function that attaches an event listener to each one, and then running another function that grabs the textArea value and runs the transformText function.
+filterInputs.forEach(input =>
+  input.addEventListener('input', () => {
+    transformText(textArea.value);
+  })
+);
